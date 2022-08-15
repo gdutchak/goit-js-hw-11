16b1009509846e2ecbea66359a25d7cc553cfa29
gdutchak <galyna.dutchak99@gmail.com>
2022-08-16 00:02:40 +0300

    update js

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,9 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import '../css/style.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import refs from './refs';
 import Create from './create';
 import searchImage from './searchImage';
-
-
-const refs = {
-  form: document.querySelector(".search-form"),
-  imageRef: document.querySelector(".gallery-list"),
-  button: document.querySelector(".load-more"),
-  input: document.querySelector('input')
-}
 
 let page = 1;
 let imagePerPage = 40;
@@ -33,41 +26,37 @@ function renderPage(e) {
     if (data.hits.length === 0) {
       throw new Error("Sorry, there are no images matching your search query. Please try again.", { timeout: 1500 })
     }
+    if (page > allResult && page > 1) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
     if (data.totalHits > imagePerPage) {
       refs.button.classList.remove('visually-hidden')
     }
-    if (page > allResult && data.totalHits > imagePerPage) {
-      refs.button.classList.add('visually-hidden')
-      Notify.info("We're sorry, but you've reached the end of search results.");
-    }
-
     Notify.success(`Hooray! We found ${data.totalHits} images.`);
     showImage(data.hits)
-    new SimpleLightbox('.gallery a', { /* options */ });
-    const { height: cardHeight } = refs.imageRef.firstElementChild.getBoundingClientRect();
-
+    new SimpleLightbox('.gallery a', {});
+    let { height: cardHeight } = refs.imageRef.firstElementChild.getBoundingClientRect();
+    if (page >= 2) {
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+      })
+    }
   }).catch(error => Notify.failure(error.message))
 }
-refs.button.addEventListener('click', (e) => {
-  page += 1;
-  renderPage(e)
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: "smooth",
-  })
-  SimpleLightbox.refresh()
-})
 
 refs.form.addEventListener("submit", (e) => {
   page = 1;
+  refs.imageRef.innerHTML = ''
   if (refs.input.value !== '') {
     renderPage(e)
   }
-
 })
 
-
+refs.button.addEventListener('click', (e) => {
+  page += 1;
+  renderPage(e);
+})
 
 
 
